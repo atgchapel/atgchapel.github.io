9fcb038398119ddb192635262d1c3af01bc8b883
kieti <emmatechlabs@gmail.com>
2023-04-25 10:50:10 +0300

    updates

--- _site/assets/js/main.js
+++ _site/assets/js/main.js
@@ -1,21 +1,14 @@
-(function renderBannerImages () {
-  let images, imageIndex, timer, bannerDiv;
-  images = [`/assets/images/2.jpg`,
-  `/assets/images/3.jpg`,
-  `/assets/images/4.jpg`,
-  `/assets/images/5.jpg`,
-  `/assets/images/7.jpg`,
-  `/assets/images/10.jpg`,];
-  bannerDiv = document.querySelector(`.welcome-banner`);
-  imageIndex = 0;
-  timer = 0;
-  for (let i = 0; i < images.length; i++) {
-    setTimeout (()=>{
-      bannerDiv.innerHTML = `<img src=${images[i]}>`;
-    }, timer);
-    imageIndex++;
-    timer += 7000;
+let slideIndex = 0;
+(function slideshow () {
+  let i;
+  let slides = document.getElementsByClassName("slideshow");
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";  
   }
+  slideIndex++;
+  if (slideIndex > slides.length) {slideIndex = 1}
+  slides[slideIndex - 1].style.display = 'block';
+  setTimeout(slideshow, 5000);
 })();
 
 (function renderStatementOfFaith () {
